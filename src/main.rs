216f735e7db7f719e1ddd2b767c89256bf5b172b
Yuji Kosugi <yuji.kosugi@gmail.com>
2021-12-08 19:26:51 +0900

    add day 1 solution

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use std::io::{self, BufRead};
+
 fn main() {
-    println!("Hello, world!");
+    let stdin = io::stdin();
+    let mut previous = i32::MAX;
+    let mut total = 0;
+
+    for line in stdin.lock().lines() {
+        let line_text = line.unwrap();
+        let number :i32 = line_text.parse().unwrap();
+        if number > previous {
+            total += 1;
+        }
+        previous = number;
+    }
+    println!("Answer: {}", total);
 }
